77c2a82a16e46d3059ef398d6fa6e2333a74a15a
Eduardo Jirón <emjironal@gmail.com>
2019-03-06 22:04:24 -0600

    Creación de figuras
    Se crearon los constructores de las figuras de tetris

--- Tretris/app/src/main/java/com/example/eduardo_jiron/tretris/Figura.java
+++ Tretris/app/src/main/java/com/example/eduardo_jiron/tretris/Figura.java
@@ -15,6 +15,136 @@ public class Figura
      */
     public Figura(TipoFigura idFigura)
     {
-        
+        cuadros = new ArrayList<>();
+        switch (idFigura)
+        {
+            case T:
+                crearFiguraT();
+                break;
+            case J:
+                crearFiguraJ();
+                break;
+            case L:
+                crearFiguraL();
+                break;
+            case S:
+                crearFiguraS();
+                break;
+            case Z:
+                crearFiguraZ();
+                break;
+            case O:
+                crearFiguraO();
+                break;
+            case I:
+                crearFiguraI();
+                break;
+        }
+    }
+
+    /**
+     * Crear la figura T
+     */
+    private void crearFiguraT()
+    {
+        /*
+        ooo
+         o
+         */
+        cuadros.add(new Cuadro(0, 0));
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(0, 2));
+        cuadros.add(new Cuadro(1, 1));
+    }
+
+    /**
+     * Crear la figura J
+     */
+    private void crearFiguraJ()
+    {
+        /*
+         o
+         o
+        oo
+         */
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(1, 1));
+        cuadros.add(new Cuadro(2, 1));
+        cuadros.add(new Cuadro(2, 0));
+    }
+
+    /**
+     * Crear la figura L
+     */
+    private void crearFiguraL()
+    {
+        /*
+        o
+        o
+        oo
+         */
+        cuadros.add(new Cuadro(0, 0));
+        cuadros.add(new Cuadro(1, 0));
+        cuadros.add(new Cuadro(2, 0));
+        cuadros.add(new Cuadro(2, 1));
+    }
+
+    /**
+     * Crear la figura S
+     */
+    private void crearFiguraS()
+    {
+        /*
+         oo
+        oo
+         */
+        cuadros.add(new Cuadro(1, 0));
+        cuadros.add(new Cuadro(1, 1));
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(0, 2));
+    }
+
+    /**
+     * Crear la figura Z
+     */
+    private void crearFiguraZ()
+    {
+        /*
+        oo
+         oo
+         */
+        cuadros.add(new Cuadro(0, 0));
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(1, 1));
+        cuadros.add(new Cuadro(1, 2));
+    }
+
+    /**
+     * Crear la figura O
+     */
+    private void crearFiguraO()
+    {
+        /*
+        oo
+        oo
+         */
+        cuadros.add(new Cuadro(0, 0));
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(1, 0));
+        cuadros.add(new Cuadro(1, 1));
+    }
+
+    /**
+     * Crear la figura I
+     */
+    private void crearFiguraI()
+    {
+        /*
+        oooo
+         */
+        cuadros.add(new Cuadro(0, 0));
+        cuadros.add(new Cuadro(0, 1));
+        cuadros.add(new Cuadro(0, 2));
+        cuadros.add(new Cuadro(0, 3));
     }
 }
